9e202b5d5ce0954e1bdf402697c6a6b18eb48f80
sandeepmaurya <sandeep_maurya@gap.com>
2023-02-14 16:47:15 +0530

    - Model improvements.

--- diabetes_classification/train.py
+++ diabetes_classification/train.py
@@ -3,5 +3,5 @@ from sklearn.linear_model import LogisticRegression
 
 
 def train_model(X_train, y_train):
-    model = LogisticRegression(solver="liblinear").fit(X_train, y_train)
+    model = LogisticRegression(C=10, solver="liblinear").fit(X_train, y_train)
     return model
