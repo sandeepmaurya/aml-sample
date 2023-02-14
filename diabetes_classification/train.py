4bed5790357d47f5e921c03c8ffe5637c18ef931
sandeepmaurya <sandeep_maurya@gap.com>
2023-02-14 11:34:53 +0530

    - Refining model

--- diabetes_classification/train.py
+++ diabetes_classification/train.py
@@ -3,7 +3,5 @@ from sklearn.linear_model import LogisticRegression
 
 
 def train_model(X_train, y_train):
-    reg_strength = 0.1
-    mlflow.log_param("reg_strength", reg_strength)
-    model = LogisticRegression(C=1 / reg_strength, solver="liblinear").fit(X_train, y_train)
+    model = LogisticRegression(solver="liblinear").fit(X_train, y_train)
     return model
